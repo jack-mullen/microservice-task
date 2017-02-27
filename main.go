00d8026997643443e2eeda0764fa1c504c3d2d88
Oke Ugwu <oke.ugwu@justdevelop.it>
2017-02-27 14:39:30 +0000

    fully implemented main func to provide 2 entries points. one for the server and another for the consumer

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"os"
+	"fmt"
 	"microservice-task/queue"
+	"strconv"
+	"microservice-task/utils"
 )
 
 var QueueProvider queue.Provider
@@ -12,9 +15,23 @@ var program = os.Getenv("U2P_PROGRAM")
 var port = os.Getenv("U2P_SERVICE_PORT")
 
 func main() {
+	action := "no-action"
+	if len(os.Args) > 1 {
+		action = os.Args[1]
+	}
+
 	QueueProvider = NewRabbitMqProvider()
 	defer QueueProvider.Cleanup()
+
+	switch action{
+	case "server":
+		portInt, err := strconv.Atoi(port)
+		utils.FailOnError(err, "Port number must be numeric")
+		startServer(portInt)
+	case "consume":
+		consumer := NewUrl2PdfConsumer(outputDir, program)
+		QueueProvider.Consume(consumer)
+	default:
+		fmt.Println("Valid options are: [server | consume]")
+	}
 }
-
-
-
