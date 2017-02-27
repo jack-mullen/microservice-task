99cda182a72e46a873ed4c32f43cd344e5c67cfa
Oke Ugwu <oke.ugwu@justdevelop.it>
2017-02-27 14:35:21 +0000

    implemented url2pdf service to create job, queue it up and serve up pdf on job completion

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"os"
 	"microservice-task/queue"
 )
 
 var QueueProvider queue.Provider
+
+var outputDir = os.Getenv("U2P_OUTPUT_DIR")
+var program = os.Getenv("U2P_PROGRAM")
+var port = os.Getenv("U2P_SERVICE_PORT")
 
 func main() {
 	QueueProvider = NewRabbitMqProvider()
