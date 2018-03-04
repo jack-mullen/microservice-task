b2262fe05a55cfe0486ccfcf2b3c64760aaec682
Jack Mullen <jack@jcmcreate.co.uk>
2018-03-04 23:07:24 +0000

    added some (basic) validation to /users post endpoint

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const config = require('./config');
 const express = require('express');
 const bodyParser = require("body-parser");
 
+const expressValidator = require('express-validator');
+
 // Constants
 const port = config.port;
 const host = '0.0.0.0';
@@ -14,6 +16,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(expressValidator());
 
 //Routes
 const routes = require('./routes');
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,12 +5,24 @@ const userCollection = db.get('users');
 const users = require('express').Router();
 
 users.post('/', (req, res) => {
-  let post = req.body;
+  let post = req.body; //request data
 
+  req.checkBody("email", "Enter a valid email address.").isEmail();
+  req.checkBody("firstName", "Enter a first name").isAlphanumeric();
+  req.checkBody("lastName", "Enter a last name").isAlphanumeric();
+
+  // If there are errors, send them out and stop processing
+  let errors = req.validationErrors();
+  if (errors) {
+    res.send(errors);
+    return;
+  }
+
+  // insert data into the 'users' collection
   userCollection.insert(
       {firstName: post.firstName, lastName: post.lastName, email: post.email }
   ).then((data) => {
-      res.json(data);
+    res.json(data);
   }).catch((err) => {
     //TODO implement better error handling
     console.error(err);
