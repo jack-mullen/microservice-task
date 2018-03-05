916e558d64d572f53a5fc530c8efa9fa18a5f813
Jack Mullen <jack@jcmcreate.co.uk>
2018-03-05 22:00:26 +0000

    added ability to delete users

--- api/index.js
+++ api/index.js
@@ -14,6 +14,13 @@ const host = '0.0.0.0';
 // App
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(expressValidator());
--- api/routes/users.js
+++ api/routes/users.js
@@ -4,6 +4,34 @@ const userCollection = db.get('users');
 
 const users = require('express').Router();
 
+users.get('/', (req, res) => {
+  //Pagination. Use if set, otherwise set to 1
+  req.query.page = req.query.page || 1;
+
+  //Return all users
+  userCollection.find({}, {
+    "limit": 10,
+    "skip": (10 * req.query.page) - 10
+  }).then((data) => {
+    res.json(data);
+  });
+
+});
+
+users.get('/:id', (req, res) => {
+  let id = req.params.id;
+  //Find user by Mongo ID
+  userCollection.find({"_id": id}, { castIds: false }).then((data) => {
+    res.json(data);
+    //TODO return a message for an invalid/unknown id
+  }).catch((err) => {
+    res.json({"error": "Mongo connection failed. Please try again"});
+    //TODO implement better error handling
+    console.error(err);
+  });
+
+});
+
 users.post('/', (req, res) => {
   let post = req.body; //request data
 
@@ -28,6 +56,16 @@ users.post('/', (req, res) => {
     console.error(err);
   }).then(() => db.close());
 
+});
+
+users.delete('/:id', (req, res) => {
+  let id = req.params.id;
+  userCollection.remove({"_id": id }).then(data => {
+    res.json(data);
+  }).catch((err) => {
+    //TODO implement better error handling
+    console.error(err);
+  }).then(() => db.close());
 });
 
 module.exports = users;
